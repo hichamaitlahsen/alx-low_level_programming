67442b9233171adcec84f99739a95bfbe846bf2c
hicham ait lahsen <hichamaitlahsen5@gmail.com>
2023-02-17 15:55:47 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * the computer it is compiled and run on ..
  * Return: Always 0.
 */
-int moin(void)
+int main(void)
 {
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
