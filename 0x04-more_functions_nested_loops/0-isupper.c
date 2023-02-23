5cca584888bfe72246df0f906cc91e49bc4ab48d
hicham ait lahsen <hichamaitlahsen5@gmail.com>
2023-02-23 08:24:58 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 
 /**
  * _isupper - check if a letter is upper
- *@x:The number to be checked
+ * @x:The number to be checked
  * Return: 1 if is upper and 0 if not
  */
 int _isupper(int x)
 {
-	if(x>=65 && x<=98)
+	if (x >= 65 && x <= 98)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
