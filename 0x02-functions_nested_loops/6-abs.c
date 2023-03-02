f499f01b66f5fabf09d318e074677b4f1110e38c
hicham ait lahsen <hichamaitlahsen5@gmail.com>
2023-03-02 08:25:15 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,37 +7,9 @@
  */
 int _abs(int n)
 {
-	if (n >= 0)
-	{
-		return (n);
-	}
-	return (-n);
-}
-
-
-7-print_last_digit.c
-
-#include "main.h"
-
-/**
- * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
- * Return: value of the last digit
- */
-int print_last_digit(int n)
+if (n >= 0)
 {
-	int a;
-
-	if (n < 0)
-	n = -n;
-
-	a = n % 10;
-
-	if (a < 0)
-		a = -a;
-
-	_putchar(a + '0');
-
-	return (a);
+return (n);
 }
-
+return (-n);
+}
