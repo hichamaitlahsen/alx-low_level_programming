e1ffec3923876c3662d1bbc8c6dc1a6c2f00e7f1
hichamaitlahsen <hichamaitlahsen5@gmail.com>
2023-06-19 17:12:17 -0700

    task o

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check if a letter is upper
+ * @x:The number to be checked
+ * Return: 1 if is upper and 0 if not
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
